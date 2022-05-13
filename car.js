065f9372f87c913cc3e4c9547ff71b9a7891b660
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2022-05-13 14:54:18 +0200

    add intersection check

--- car.js
+++ car.js
@@ -17,10 +17,10 @@ class Car
     this.controls = new Controls();
   }
 
-  update()
+  update( roadBorders )
   {
     this.#move();
-    this.sensor.update();
+    this.sensor.update( roadBorders );
   }
 
   #move()
