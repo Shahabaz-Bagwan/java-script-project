5edf71da547a724424abfd9a0fdb1146b3a97013
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2022-05-19 09:45:01 +0200

    add json reader

--- utils.js
+++ utils.js
@@ -47,8 +47,22 @@ function getRGBA(value) {
 
 }
 
-
 function getRandomColor() {
   const hue = 290 + Math.random() * 260;
   return "hsl(" + hue + ", 100%, 60%)";
+}
+
+// load local json using pure js
+function loadJSON(file, callback) {
+
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', file, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
 }
